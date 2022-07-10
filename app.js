3fab99cc88a46d5a7911929ae33921ba93c878ca
dskowronski-dotcomp <d.skowronski@dotcomp.pl>
2022-07-10 21:53:16 +0200

    configure nodemailer - 1st commit

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
-//const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 const routes = require('./routes/index');
 
 const app = express();
 
-app.post('/send', (req, res) => {
-    console.log('test');
-})
+// app.post('/send', (req, res) => {
+//     console.log('test');
+// })
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,6 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+// mail sender
+app.use(express.json());
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        sendmail: true,
+        newline: 'unix',
+        path: '/usr/sbin/sendmail'
+    })
+    transporter.sendMail({
+        from: email,
+        to: 'dskowronski97@gmail.com',
+        subject: 'Nowa wiadomość',
+        text: message
+    }, (err, info) => {
+        console.log(info.envelope);
+        console.log(info.messageId);
+    })
+})
 
 app.use(flash());
 
