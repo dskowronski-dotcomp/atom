9181de2eb750614e6af04e1ee44ad0d21938a5f9
dskowronski-dotcomp <d.skowronski@dotcomp.pl>
2022-07-26 19:39:53 +0200

    try fix mailsender x3

--- app.js
+++ app.js
@@ -23,43 +23,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // mail sender
 
-const contactForm = document.querySelector('.contact-form');
+// const contactForm = document.querySelector('.contact-form');
 
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let phone = document.getElementById('phone');
-let message = document.getElementById('message');
+// let name = document.getElementById('name');
+// let email = document.getElementById('email');
+// let phone = document.getElementById('phone');
+// let message = document.getElementById('message');
 
 
-contactForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
+// contactForm.addEventListener("submit", (e)=>{
+//     e.preventDefault();
 
-    let formData = {
-        name: name.value,
-        email: email.value,
-        phone: phone.value,
-        message: message.value
-    }
+//     let formData = {
+//         name: name.value,
+//         email: email.value,
+//         phone: phone.value,
+//         message: message.value
+//     }
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function(){
-        console.log(xhr.responseText);
-        if(xhr.responseText == 'success'){
-            alert('Wiadomość została wysłana');
-            name.value = '';
-            email.value = '';
-            phone.value = '';
-            message.value = '';
-        }else{
-            alert('Coś poszło nie tak! Spróbuj ponownie za chwilę.');
-        }
-    }
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('POST', '/');
+//     xhr.setRequestHeader('content-type', 'application/json');
+//     xhr.onload = function(){
+//         console.log(xhr.responseText);
+//         if(xhr.responseText == 'success'){
+//             alert('Wiadomość została wysłana');
+//             name.value = '';
+//             email.value = '';
+//             phone.value = '';
+//             message.value = '';
+//         }else{
+//             alert('Coś poszło nie tak! Spróbuj ponownie za chwilę.');
+//         }
+//     }
 
-    xhr.send(JSON.stringify(formData));
+//     xhr.send(JSON.stringify(formData));
 
-});
+// });
 
 
 
