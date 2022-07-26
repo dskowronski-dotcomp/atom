8ec824ebb768cd218ba9a4b10f3a8a064b54875c
dskowronski-dotcomp <d.skowronski@dotcomp.pl>
2022-07-26 20:30:00 +0200

    try fix email

--- public/js/mailsend.js
+++ public/js/mailsend.js
@@ -1,10 +1,10 @@
+
 const contactForm = document.querySelector('.contact-form');
 
 let name = document.getElementById('name');
 let email = document.getElementById('email');
 let phone = document.getElementById('phone');
 let message = document.getElementById('message');
-
 
 contactForm.addEventListener("submit", (e)=>{
     e.preventDefault();
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const app = require('./app');
+//const app = require('./app');
+
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 const nodemailer = require('nodemailer');
+const routes = require('./routes/index');
+
+const app = express();
+
+// app.post('/send', (req, res) => {
+//     console.log('test');
+// })
 
 
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+// mail sender
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname + '/public/contact.pug')
+// })
 
 app.use(express.json());
-app.post('/', (req, res)=>{
+app.post('/public/contact.pug', (req, res)=>{
     console.log(req.body);
 
     const transporter = nodemailer.createTransport({
@@ -39,6 +63,11 @@ app.post('/', (req, res)=>{
     })
 })
 
+app.use(flash());
+
+app.use('/', routes);
+
+module.exports = app;
 
 
 
