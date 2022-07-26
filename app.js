f4bbea6363526beba0bcfa79e4bb9e1e67a4be7e
dskowronski-dotcomp <d.skowronski@dotcomp.pl>
2022-07-27 00:24:06 +0200

    off mailsender

--- app.js
+++ app.js
@@ -23,79 +23,79 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // mail sender v2
 
-const contactForm = document.querySelector('.contact-form');
+// const contactForm = document.querySelector('.contact-form');
 
-contactForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
+// contactForm.addEventListener("submit", (e)=>{
+//     e.preventDefault();
 
-    const transporter = nodemailer.createTransport({
-        host: "mail.atom-service.pl",
-        port: 587,
-        secure: false, // upgrade later with STARTTLS
-        auth: {
-          user: "kontakt@atom-service.pl",
-          pass: "1stPassword",
-        },
-    })
+//     const transporter = nodemailer.createTransport({
+//         host: "mail.atom-service.pl",
+//         port: 587,
+//         secure: false, // upgrade later with STARTTLS
+//         auth: {
+//           user: "kontakt@atom-service.pl",
+//           pass: "1stPassword",
+//         },
+//     })
     
-    const mailOptions = {
-        from: req.body.email,
-        to: "kontakt@atom-service.pl",
-        subject: "Message from",
-        text: req.body.message
-    }
+//     const mailOptions = {
+//         from: req.body.email,
+//         to: "kontakt@atom-service.pl",
+//         subject: "Message from",
+//         text: req.body.message
+//     }
     
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(error){
-            console.log(error);
-            res.send('error');
-        } else{
-            console.log('Email sent: ' + info.response);
-            res.send('success');
-        }
-         console.log(info.envelope);
-         console.log(info.messageId);
-    })
+//     transporter.sendMail(mailOptions, (err, info) => {
+//         if(error){
+//             console.log(error);
+//             res.send('error');
+//         } else{
+//             console.log('Email sent: ' + info.response);
+//             res.send('success');
+//         }
+//          console.log(info.envelope);
+//          console.log(info.messageId);
+//     })
     
 
-});
+// });
 
 
-// mail sender 
+// // mail sender 
 
-app.use(express.json());
-app.post('/', (req, res)=>{
-    console.log(req.body);
+// app.use(express.json());
+// app.post('/', (req, res)=>{
+//     console.log(req.body);
 
-    const transporter = nodemailer.createTransport({
-        host: "mail.atom-service.pl",
-        port: 587,
-        secure: false, // upgrade later with STARTTLS
-        auth: {
-          user: "kontakt@atom-service.pl",
-          pass: "1stPassword",
-        },
-    })
+//     const transporter = nodemailer.createTransport({
+//         host: "mail.atom-service.pl",
+//         port: 587,
+//         secure: false, // upgrade later with STARTTLS
+//         auth: {
+//           user: "kontakt@atom-service.pl",
+//           pass: "1stPassword",
+//         },
+//     })
 
-    const mailOptions = {
-        from: req.body.email,
-        to: "kontakt@atom-service.pl",
-        subject: "Message from",
-        text: req.body.message
-    }
+//     const mailOptions = {
+//         from: req.body.email,
+//         to: "kontakt@atom-service.pl",
+//         subject: "Message from",
+//         text: req.body.message
+//     }
 
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(error){
-            console.log(error);
-            res.send('error');
-        } else{
-            console.log('Email sent: ' + info.response);
-            res.send('success');
-        }
-         console.log(info.envelope);
-         console.log(info.messageId);
-    })
-})
+//     transporter.sendMail(mailOptions, (err, info) => {
+//         if(error){
+//             console.log(error);
+//             res.send('error');
+//         } else{
+//             console.log('Email sent: ' + info.response);
+//             res.send('success');
+//         }
+//          console.log(info.envelope);
+//          console.log(info.messageId);
+//     })
+// })
 
 app.use(flash());
 
