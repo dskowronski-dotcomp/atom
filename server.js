64cd8075a07939c92c0729f8980e7c223663ae06
dskowronski-dotcomp <d.skowronski@dotcomp.pl>
2022-08-03 01:06:20 +0200

    change port

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ module.exports = app;
 
 
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening on ${ server.address().port }`);
